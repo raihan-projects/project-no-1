05a8d637cba1dd83008c4a34ae6e59724a527585
raihan-ahmed-bd <ahmed9.raihan@gmail.com>
2024-09-08 16:09:28 +0600

    update

---
+++ no1 navbar/function.js
@@ -0,0 +1,8 @@
+function menu() {
+  const navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("menu-active");
+}
+function search() {
+    const searchBox = document.querySelector(".search");
+    searchBox.classList.toggle("search-active");
+}
